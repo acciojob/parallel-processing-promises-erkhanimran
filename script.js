66f2209ac3aba277d1078f43ce0864d642ada737
Khan Imran <98589155+erkhanimran@users.noreply.github.com>
2023-08-08 14:14:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,53 +1,46 @@
 //your JS code here. If required.
-const res = document.getElementById("output");
+const output = document.getElementById("output");
+const btn = document.getElementById("download-images-button");
 
-const promises = [
-  new Promise((resolve) => {
-    const time = Math.floor(Math.random() * 3 + 1) * 1000;
-    setTimeout(() => resolve({ name: "Promise 1", time: time / 1000 }), time);
-  }),
-  new Promise((resolve) => {
-    const time = Math.floor(Math.random() * 3 + 1) * 1000;
-    setTimeout(() => resolve({ name: "Promise 2", time: time / 1000 }), time);
-  }),
-  new Promise((resolve) => {
-    const time = Math.floor(Math.random() * 3 + 1) * 1000;
-    setTimeout(() => resolve({ name: "Promise 3", time: time / 1000 }), time);
-  }),
+const images = [
+  { url: "https://picsum.photos/id/237/200/300" },
+  { url: "https://picsum.photos/id/238/200/300" },
+  { url: "https://picsum.photos/id/239/200/300" },
 ];
 
-async function callFns() {
-  const start = new Date();
-  // Use Promise.all to wait for all Promises to resolve
-  res.innerHTML += `
-            <tr id="loading">
-                <td colspan=2>Loading...</td>
-            </tr>
-          `;
-  await Promise.all(promises)
-    .then((results) => {
-      res.innerHTML = ``;
-      // Log the array of results
-      results.forEach((e) => {
-        res.innerHTML += `
-            <tr>
-                <td>${e.name}</td>
-                <td>${e.time}</td>
-            </tr>
-          `;
-      });
-    })
-    .catch((error) => {
-      console.error(error);
-    });
 
-  const end = new Date();
-  const timeInMillis = end - start;
-  res.innerHTML += `
-            <tr>
-                <td>Total</td>
-                <td>${timeInMillis / 1000}</td>
-            </tr>
-          `;
+function downloadImages() {
+
+ let promises = images.map((val)=>{
+			
+		   let prom = new Promise((resolve,reject)=>{
+			   const img = new Image();
+			   img.src = val.url;
+			   img.alt = val.alt;
+ 			img.onload = ()=>{
+				resolve(img);
+			}
+
+			img.onerror = ()=>{
+				reject(`Failed to load image's URL: ${val.url}`)
+			}
+		});
+	 return prom;
+	});
+
+	 let x = Promise.all(promises)
+	x.then((data) =>{
+		data.forEach((image) =>{
+			output.appendChild(image);
+		})
+	})
+	x.catch((data)=>
+		{
+			console.log(data);
+		});
+
 }
-callFns();
+
+
+btn.addEventListener("click" , downloadImages);
+
